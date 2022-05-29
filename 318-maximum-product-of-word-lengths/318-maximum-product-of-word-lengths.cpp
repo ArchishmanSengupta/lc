068691d01ccb27f28857d2ca2427f528bc4ea4e1
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-05-29 22:39:35 +0530

    Time: 166 ms (31.62%), Space: 15.5 MB (94.99%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int maxlen = 0;
+        for(int i = 0; i < words.size(); i++)
+            for(int j = i + 1; j < words.size(); j++){
+                if(words[i].size() * words[j].size() <= maxlen) continue;
+                if(noCommon(words[i], words[j])) maxlen = max(maxlen, (int)(words[i].size() * words[j].size()));
+            }
+        return maxlen;
+    }
+    
+    bool noCommon(string& a, string& b){
+        for(auto x: a) 
+            for(auto y: b)
+                if(x == y) return false;
+        return true;
+    }
+        
+};
