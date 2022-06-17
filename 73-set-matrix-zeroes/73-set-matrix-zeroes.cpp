1d0910a44cf94d670afe8637fa7e62ef50cecce2
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-06-17 12:30:01 +0530

    Time: 28 ms (22.84%), Space: 13.3 MB (53.28%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        const int H = matrix.size(); //row
+             const int H = matrix.size(); //row
         const int W = matrix[0].size(); // col
             
-            vector<bool> row_zero(H);
+            // vector<bool> row_zero(H);
             vector<bool> col_zero(W);
             
             for(size_t row = 0; row<H; row++){
                     for(size_t col = 0; col<W; col++){
                             if(matrix[row][col] == 0){
-                                    row_zero[row] = true;
+                                    // row_zero[row] = true;
                                     col_zero[col] = true;
                             }
                     }
             }
             for(size_t row = 0; row<H; row++){
+                    bool contains_zero = false;
+                    for(int col = 0; col<W; col++){
+                            if(matrix[row][col] == 0){
+                                    contains_zero = true;
+                            }
+                    }
                     for(size_t col = 0; col<W; col++){
-                            if(row_zero[row] || col_zero[col]){
+                            if(contains_zero || col_zero[col]){
                                     matrix[row][col]=0;
                             }
                     }
             }
-            
-            
-            
     }
 };
