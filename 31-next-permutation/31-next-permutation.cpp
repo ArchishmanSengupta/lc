3e5c31a1e395e568767867632ca2385acdbf1741
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-06-18 23:32:40 +0530

    Time: 9 ms (29.20%), Space: 12.2 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(begin(nums), end(nums));
+       
+            int n = nums.size()-1;
+            int mini = INT_MAX;
+            int breakingPoint = 0;
+            
+            // from back get the breaking point
+            for(int i = n; i>0; i--){
+                    if(nums[i] > nums[i-1]){
+                            breakingPoint = i;
+                            break;
+                    }
+            }
+            // base case 
+            if(breakingPoint == 0){
+                    sort(nums.begin(),nums.end());
+            }
+            else{
+                    int numToSwap = nums[breakingPoint - 1];
+                    
+                    for(int j=breakingPoint; j<=n;j++){
+                        
+                            if(nums[j] - numToSwap > 0 and nums[j] - numToSwap < mini){
+                               int swapping = nums[j];
+                                    nums[j] = nums[breakingPoint - 1];
+                                    nums[breakingPoint - 1] = swapping;
+                            }
+                    }
+                    
+                    sort(nums.begin() + breakingPoint, nums.end());
+            }
     }
 };
