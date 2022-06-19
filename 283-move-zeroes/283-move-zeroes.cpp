cac1771638bf0b94cba4580b4251e8c191b011f7
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-06-19 19:38:06 +0530

    Time: 29 ms (71.19%), Space: 19.3 MB (57.41%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-            
-            int pointer = 0;
-            for(int i=0;i<nums.size();i++){
-                    if(nums[i]!=0){
-                            swap(nums[i], nums[pointer]);
+            int n = nums.size();
+            int pointer=0;
+            for(auto i: nums)
+            {
+                    if(i != 0){
+                         nums[pointer] = i;  
                             pointer++;
                     }
             }
-            
-            
+             for(int i=pointer;i<n;i++){
+                            nums[i] =0;
+                    }
     }
 };
