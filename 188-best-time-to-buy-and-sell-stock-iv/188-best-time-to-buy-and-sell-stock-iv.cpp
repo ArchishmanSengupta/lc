61598bf0d5761f008db0f244544b9dd212e989a9
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-07-15 23:27:50 +0530

    Time: 68 ms (14.83%), Space: 21.3 MB (6.08%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,36 +1,28 @@
-class Solution {
-public:
-        int profit(vector<int>& prices, int i, int n, int k, int holding, vector<vector<vector<int>>> &dp){
-                
+class Solution{
+  public:
+        
+        int profit(vector<int>&prices, int i, int n, int k, vector<vector<vector<int>>>&dp,int holding){
                 if(i>=n) return 0;
                 if(k==0) return 0;
-                
                 if(dp[i][k][holding] !=-1) return dp[i][k][holding];
                 
-                int doNothing = profit(prices, i+1,n,k,holding,dp);
+        int doNothing = profit(prices, i+1, n, k, dp,holding);
                 
                 if(holding == 0){
-                        //holding = 1;
-                      int buy = profit(prices,i+1,n,k,1,dp) - prices[i]; 
+                        int buy = profit(prices, i+1, n, k,dp,1) - prices[i];
                         return dp[i][k][holding] = max(buy,doNothing);
                 }
-                else
-                {
-                        // holding = 0;
-                        int sell = profit(prices, i+1,n,k-1,0,dp) + prices[i]; 
-                        return dp[i][k][holding] = max(sell,doNothing);
-                }   
-                
+        else{
+                int sell = profit(prices, i+1,n,k-1, dp,0) + prices[i];
+                return dp[i][k][holding] = max(sell, doNothing);
         }
-    int maxProfit(int k, vector<int>& prices) {
-        int profit1 = INT_MAX;
-            int n = size(prices);
-            int i=0;
-            bool holding=0;
-            // k=k*2;
-            
-            vector<vector<vector<int>>> dp(n,vector<vector<int>>(k+1,vector<int>(2,-1)));
-            return profit(prices,i,n,k,holding,dp);
-            
-    }
+        }
+        int maxProfit(int k, vector<int>&prices){
+                int n = size(prices);
+                int i = 0;
+                int holding =0;
+                vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(k+1,vector<int>(2,-1)));
+                
+                return profit(prices,i, n,k,dp,holding);
+        }
 };
