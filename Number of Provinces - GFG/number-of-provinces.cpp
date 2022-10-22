5d3caf706c4d7b6be6426898a74eedb8f4a756df
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2022-10-23 00:53:28 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -11,6 +11,7 @@ class Solution {
   void dfs(int i, vector<int> adjls[], int visited[]){
         //mark the node as visited
         visited[i] = 1;
+        
         for(auto it:adjls[i]){
             if(!visited[it]){
                 dfs(it, adjls,visited);
@@ -19,9 +20,12 @@ class Solution {
     }
     int numProvinces(vector<vector<int>> adj, int V) {
         // Changing the adjacency matrix to Adjacency List
+        
         vector<int> adjls[V];
         for(int i=0;i<V;i++){
             for(int j=0;j<V;j++){
+                //CHECK IF THERE ARE NO SELF LOOPS
+                
                 if(adj[i][j]==1 and i!=j){
                     adjls[i].push_back(j);
                     adjls[j].push_back(i);
@@ -29,17 +33,25 @@ class Solution {
             }
         }
         
-        //Mak
+        //Making a adjacency List
         int visited[V]={0};
         
+        //Initializing the counter variable which stores the number of provinces
         int counter = 0;
+        
         for(int i=0;i<V;i++){
+            //CHECK IF THE NODE IS NOT VISITED
+            
             if(!visited[i]){
+                
+                //INCREMENT COUNTER TO COUNTER EACH STARTING NODE
                 counter++;
+                //CALL THE DFS FUNCTION FOR THE REST OF THE NODES
                 dfs(i,adjls,visited);
             }
         }
         
+        //RETURN COUNTER
         return counter;
     }
 };
